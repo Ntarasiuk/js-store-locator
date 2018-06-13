2e7980ce9dd75e0cc907da7fd6f7ac5ed859ebf4
Nathan Tarasiuk <Ntarasiuk@gmail.com>
2018-06-12 21:57:41 -0700

    changed zoom on map

--- coastpump/coastpump.js
+++ coastpump/coastpump.js
@@ -5,7 +5,7 @@ var coastpumplocs = [
     "title": "13411 Chambord St",
     "desc": "Phone: (352)-597-5800<br>Fax: (352)-597-5552",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 8,
+    "zoom": 5,
     "html": "<b>13411 Chambord St</b></br>Phone: (352)-597-5800<br>Fax: (352)-597-5552"
   },
   {
@@ -14,25 +14,25 @@ var coastpumplocs = [
     "title": "2801 NW 112th Ave",
     "desc": "Phone: (305)593-0101 <br>Fax: (305) 592-4782 <br>Asst Mgr: Reyder (Ray) Roman<br>Regional: Marlon Sirju",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 8,
+    "zoom": 5,
     "html": "<b>2801 NW 112th Ave</b></br>Phone: (305)593-0101 <br>Fax: (305) 592-4782 <br>Asst Mgr: Reyder (Ray) Roman<br>Regional: Marlon Sirju"
   },
   {
     "lon": -81.9517981,
-    "lat": 26.6619498,
+    "lat": 26.6619495,
     "title": "1110 NE Pine Island Rd #17",
     "desc": "Phone: (239)-772-1151<br>Fax: (239)-772-1161<br>Mgr: Tom Trendafilov<br>Regional Mgr: Ron Reagan",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 8,
+    "zoom": 5,
     "html": "<b>1110 NE Pine Island Rd #17</b></br>Phone: (239)-772-1151<br>Fax: (239)-772-1161<br>Mgr: Tom Trendafilov<br>Regional Mgr: Ron Reagan"
   },
   {
-    "lon": -81.8498848,
-    "lat": 26.5688808,
+    "lon": -81.8498845,
+    "lat": 26.5688805,
     "title": "6320 Topaz Ct",
     "desc": "Phone: (239)-936-1414<br>Fax: (239)-936-1704<br>Mgr: Marcus Taylor<br>Regional Mgr: Ron Reagan",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 8,
+    "zoom": 5,
     "html": "<b>6320 Topaz Ct</b></br>Phone: (239)-936-1414<br>Fax: (239)-936-1704<br>Mgr: Marcus Taylor<br>Regional Mgr: Ron Reagan"
   },
   {
@@ -41,7 +41,7 @@ var coastpumplocs = [
     "title": "632B Lovejoy Rd NW #2",
     "desc": "Fax: (850)-864-3003<br>Phone: (850)-864-4004",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 8,
+    "zoom": 5,
     "html": "<b>632B Lovejoy Rd NW #2</b></br>Fax: (850)-864-3003<br>Phone: (850)-864-4004"
   },
   {
@@ -50,16 +50,16 @@ var coastpumplocs = [
     "title": "4682 Dusk Ct",
     "desc": "Fax: (904)-636-0735<br>Phone: (904)-636-0627",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 8,
+    "zoom": 5,
     "html": "<b>4682 Dusk Ct</b></br>Fax: (904)-636-0735<br>Phone: (904)-636-0627"
   },
   {
-    "lon": -80.1988828,
+    "lon": -80.1988825,
     "lat": 26.1464755,
     "title": "3761 NW 16th St",
     "desc": "Phone: (954) 583-6202 <br>Fax: (954) 587-7096 <br>Mrg: Brian Thompson<br>Regional: Marlon Sirju",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 8,
+    "zoom": 5,
     "html": "<b>3761 NW 16th St</b></br>Phone: (954) 583-6202 <br>Fax: (954) 587-7096 <br>Mrg: Brian Thompson<br>Regional: Marlon Sirju"
   },
   {
@@ -68,7 +68,7 @@ var coastpumplocs = [
     "title": "10302 90th Trail",
     "desc": "Phone: (386) 208-0888<br>Fax: (386) 208-0684<br>Mgr: Brian Campbell <br>Regional Mgr: Brian Campbell",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 8,
+    "zoom": 5,
     "html": "<b>10302 90th Trail</b></br>Phone: (386) 208-0888<br>Fax: (386) 208-0684<br>Mgr: Brian Campbell <br>Regional Mgr: Brian Campbell"
   },
   {
@@ -77,7 +77,7 @@ var coastpumplocs = [
     "title": "1652 Longwood Ave",
     "desc": "Fax: (407)-331-0886<br>Phone: (407)-331-8989",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 8,
+    "zoom": 5,
     "html": "<b>1652 Longwood Ave</b></br>Fax: (407)-331-0886<br>Phone: (407)-331-8989"
   },
   {
@@ -86,16 +86,16 @@ var coastpumplocs = [
     "title": "7835 NW 72nd Ave",
     "desc": "Phone: (305)883-0564 <br>Fax: (305)889-1475 <br>Mgr: Andy Alvarez <br>Regional Mgr: Marlon Sirju",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 8,
+    "zoom": 5,
     "html": "<b>7835 NW 72nd Ave</b></br>Phone: (305)883-0564 <br>Fax: (305)889-1475 <br>Mgr: Andy Alvarez <br>Regional Mgr: Marlon Sirju"
   },
   {
-    "lon": -81.6797348,
-    "lat": 26.1594848,
+    "lon": -81.6797345,
+    "lat": 26.1594845,
     "title": "3576 Plover Ave",
     "desc": "Phone: (239)-352-3520<br>Fax: (239)-348-3434<br>Mgr: Richard Sollmann<br>Regional Mgr: Ron Reagan",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 8,
+    "zoom": 5,
     "html": "<b>3576 Plover Ave</b></br>Phone: (239)-352-3520<br>Fax: (239)-348-3434<br>Mgr: Richard Sollmann<br>Regional Mgr: Ron Reagan"
   },
   {
@@ -104,16 +104,16 @@ var coastpumplocs = [
     "title": "2870 NE 24th St",
     "desc": "Phone: (352)620-0616 <br>Fax: (352) 620-0982 <br>Mgr: Mike McGurk <br>Regional Mgr: Steve McCormack<br>Rick Davis Fax# (352) 629-5640",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 8,
+    "zoom": 5,
     "html": "<b>2870 NE 24th St</b></br>Phone: (352)620-0616 <br>Fax: (352) 620-0982 <br>Mgr: Mike McGurk <br>Regional Mgr: Steve McCormack<br>Rick Davis Fax# (352) 629-5640"
   },
   {
-    "lon": -82.6744608,
+    "lon": -82.6744605,
     "lat": 28.0408867,
     "title": "140 Scarlet Blvd",
     "desc": "Phone: (813)-855-2456<br>Fax: (813)-854-4834<br>Mgr: Jeff Reinig <br>Regional Mgr: John Salazar",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 8,
+    "zoom": 5,
     "html": "<b>140 Scarlet Blvd</b></br>Phone: (813)-855-2456<br>Fax: (813)-854-4834<br>Mgr: Jeff Reinig <br>Regional Mgr: John Salazar"
   },
   {
@@ -122,7 +122,7 @@ var coastpumplocs = [
     "title": "1130 12th Ave E",
     "desc": "Phone: (941)-729-5618<br>Fax: (941)-772-0638<br>Mgr: Sal Rios<br>Regional Mgr: John Salazar",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 8,
+    "zoom": 5,
     "html": "<b>1130 12th Ave E</b></br>Phone: (941)-729-5618<br>Fax: (941)-772-0638<br>Mgr: Sal Rios<br>Regional Mgr: John Salazar"
   },
   {
@@ -131,7 +131,7 @@ var coastpumplocs = [
     "title": "4241 James St",
     "desc": "Phone: (941)629-4234 <br>Fax: (941) 629-6714 <br>Mgr: Jacob Coleman <br>Regional Mgr: Ron Reagan",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 8,
+    "zoom": 5,
     "html": "<b>4241 James St</b></br>Phone: (941)629-4234 <br>Fax: (941) 629-6714 <br>Mgr: Jacob Coleman <br>Regional Mgr: Ron Reagan"
   },
   {
@@ -140,7 +140,7 @@ var coastpumplocs = [
     "title": "555 NW Enterprise Dr",
     "desc": "Phone: (772)340-3400 <br>Fax: (772) 340-3480 <br>Mgr: Eric White <br>Regional: Marlon Sirju",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 8,
+    "zoom": 5,
     "html": "<b>555 NW Enterprise Dr</b></br>Phone: (772)340-3400 <br>Fax: (772) 340-3480 <br>Mgr: Eric White <br>Regional: Marlon Sirju"
   },
   {
@@ -149,7 +149,7 @@ var coastpumplocs = [
     "title": "2690 W Airport Blvd",
     "desc": "Phone: (407) 328-0040 <br>Fax: (407) 323-8811 <br>Mgr: Craig Finnell <br>Regional Mgr: Phil Savage",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 8,
+    "zoom": 5,
     "html": "<b>2690 W Airport Blvd</b></br>Phone: (407) 328-0040 <br>Fax: (407) 323-8811 <br>Mgr: Craig Finnell <br>Regional Mgr: Phil Savage"
   },
   {
@@ -158,7 +158,7 @@ var coastpumplocs = [
     "title": "1701 Cattlemen Rd",
     "desc": "Phone: (941)-378-9554<br>Fax: (941)-378-9470<br>Training Mgr: Nick Flickinger <br>Regional Mgr: John Salazar",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 8,
+    "zoom": 5,
     "html": "<b>1701 Cattlemen Rd</b></br>Phone: (941)-378-9554<br>Fax: (941)-378-9470<br>Training Mgr: Nick Flickinger <br>Regional Mgr: John Salazar"
   },
   {
@@ -167,7 +167,7 @@ var coastpumplocs = [
     "title": "610 Groveland Ave",
     "desc": "Phone: (941)-484-3738<br>Fax: (941)-484-2631<br>Mgr: Josh Ferrill<br>Regional Mgr: Ron Reagan",
     "icon": "https://mt.googleapis.com/vt/icon/name=icons/onion/SHARED-mymaps-container-bg_4x.png,icons/onion/SHARED-mymaps-container_4x.png,icons/onion/1502-shape_star_4x.png&highlight=ff000000,FFD600",
-    "zoom": 8,
+    "zoom": 5,
     "html": "<b>610 Groveland Ave</b></br>Phone: (941)-484-3738<br>Fax: (941)-484-2631<br>Mgr: Josh Ferrill<br>Regional Mgr: Ron Reagan"
   },
   {
@@ -176,7 +176,7 @@ var coastpumplocs = [
     "title": "1750 W 10th St",
     "desc": "Phone:(561)844-5252 <br>Fax: (561) 842-8842 <br>Mgr: Todd Willis <br>Regional: Marlon Sirju",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 8,
+    "zoom": 5,
     "html": "<b>1750 W 10th St</b></br>Phone:(561)844-5252 <br>Fax: (561) 842-8842 <br>Mgr: Todd Willis <br>Regional: Marlon Sirju"
   },
   {
@@ -185,7 +185,7 @@ var coastpumplocs = [
     "title": "1652 S Ronald Reagan Blvd",
     "desc": "Phone: (407) 331-8989 <br>Fax: (407) 331-0886 <br>Mgr: Ken Galgano <br>Regional Mgr: Phil Savage",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 8,
+    "zoom": 5,
     "html": "<b>1652 S Ronald Reagan Blvd</b></br>Phone: (407) 331-8989 <br>Fax: (407) 331-0886 <br>Mgr: Ken Galgano <br>Regional Mgr: Phil Savage"
   },
   {
@@ -194,16 +194,16 @@ var coastpumplocs = [
     "title": "4682 Dusk Ct",
     "desc": "Phone: (904)636-0627<br> Fax: (904) 636-0735<br>Mgr: Gary Bussey <br>Regional Mgr: Brian Campbell",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 8,
+    "zoom": 5,
     "html": "<b>4682 Dusk Ct</b></br>Phone: (904)636-0627<br> Fax: (904) 636-0735<br>Mgr: Gary Bussey <br>Regional Mgr: Brian Campbell"
   },
   {
     "lon": -86.6567683,
-    "lat": 30.423678,
+    "lat": 30.423675,
     "title": "632 Lovejoy Rd NW #2",
     "desc": "Phone: (850) 864-4004 <br>Fax: (850) 864-3003 <br>Mgr: Dustin Jones <br>Regional Mgr: Brian Campbell",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 8,
+    "zoom": 5,
     "html": "<b>632 Lovejoy Rd NW #2</b></br>Phone: (850) 864-4004 <br>Fax: (850) 864-3003 <br>Mgr: Dustin Jones <br>Regional Mgr: Brian Campbell"
   }
 ]
