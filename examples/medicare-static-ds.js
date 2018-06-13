7f81585c66003941b2d730b876ff87000308b04f
Nathan Tarasiuk <Ntarasiuk@gmail.com>
2018-06-12 20:12:25 -0700

    changed features

--- examples/medicare-static-ds.js
+++ examples/medicare-static-ds.js
@@ -11,7 +11,22 @@ function MedicareDataSource() {
   });
 }
 
+/**
+ * @const
+ * @type {!storeLocator.FeatureSet}
+ * @private
+ */
+MedicareDataSource.prototype.FEATURES_ = new storeLocator.FeatureSet(
+  new storeLocator.Feature('Wheelchair-YES', 'Wheelchair access'),
+  new storeLocator.Feature('Audio-YES', 'Audio')
+);
 
+/**
+ * @return {!storeLocator.FeatureSet}
+ */
+MedicareDataSource.prototype.getFeatures = function() {
+  return this.FEATURES_;
+};
 
 /**
  * @private
@@ -25,15 +40,15 @@ MedicareDataSource.prototype.parse_ = function(csv) {
 
   for (var i = 1, row; row = rows[i]; i++) {
     row = this.toObject_(headings, this.parseRow_(row));
+    var features = new storeLocator.FeatureSet;
+    features.add(this.FEATURES_.getById('Wheelchair-' + row.Wheelchair));
+    features.add(this.FEATURES_.getById('Audio-' + row.Audio));
 
     var position = new google.maps.LatLng(row.Y, row.X);
 
-    var shop = this.join_([row.Shp_num_an, row.Shp_centre], ', ');
-    var locality = this.join_([row.Locality, row.Postcode], ', ');
-
     var store = new storeLocator.Store(row.uuid, position, features, {
       title: row.Name,
-      address: row.Name,
+      address: Name
     });
     stores.push(store);
   }
