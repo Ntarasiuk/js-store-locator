5b20cd8c95d5d757906da8465f3841c7e7648367
Nathan Tarasiuk <Ntarasiuk@gmail.com>
2018-07-30 18:59:07 -0700

    updated zoom

--- coastpump/coastpump.js
+++ coastpump/coastpump.js
@@ -6,7 +6,7 @@ var coastpumplocs = [
     "street": "13411 Chambord St, Brooksville, FL 34613",
     "desc": "Phone: (352)-597-5800<br>Fax: (352)-597-5552",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 4,
+    "zoom": 7,
     "html": "<b>Brooksville, FL</b></br>13411 Chambord St, Brooksville, FL 34613</br>Phone: (352)-597-5800<br>Fax: (352)-597-5552"
   },
   {
@@ -16,7 +16,7 @@ var coastpumplocs = [
     "street": "2801 NW 112th Ave, Doral, FL 33172",
     "desc": "Phone: (305)593-0101 <br>Fax: (305) 592-4782 <br>Asst Mgr: Reyder (Ray) Roman<br>Regional: Marlon Sirju",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 4,
+    "zoom": 7,
     "html": "<b>Doral, FL</b></br>2801 NW 112th Ave, Doral, FL 33172</br>Phone: (305)593-0101 <br>Fax: (305) 592-4782 <br>Asst Mgr: Reyder (Ray) Roman<br>Regional: Marlon Sirju"
   },
   {
@@ -26,7 +26,7 @@ var coastpumplocs = [
     "street": "1110 NE Pine Island Rd #17, Cape Coral, FL, 33909",
     "desc": "Phone: (239)-772-1151<br>Fax: (239)-772-1161<br>Mgr: Tom Trendafilov<br>Regional Mgr: Ron Reagan",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 4,
+    "zoom": 7,
     "html": "<b>Cape Coral, FL</b></br>1110 NE Pine Island Rd #17, Cape Coral, FL, 33909</br>Phone: (239)-772-1151<br>Fax: (239)-772-1161<br>Mgr: Tom Trendafilov<br>Regional Mgr: Ron Reagan"
   },
   {
@@ -36,7 +36,7 @@ var coastpumplocs = [
     "street": "6320 Topaz Ct, Fort Myers, FL 33966",
     "desc": "Phone: (239)-936-1414<br>Fax: (239)-936-1704<br>Mgr: Marcus Taylor<br>Regional Mgr: Ron Reagan",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 4,
+    "zoom": 7,
     "html": "<b>Fort Myers, FL</b></br>6320 Topaz Ct, Fort Myers, FL 33966</br>Phone: (239)-936-1414<br>Fax: (239)-936-1704<br>Mgr: Marcus Taylor<br>Regional Mgr: Ron Reagan"
   },
   {
@@ -46,7 +46,7 @@ var coastpumplocs = [
     "street": "632B Lovejoy Rd NW #2, Fort Walton Beach, 32548",
     "desc": "Fax: (850)-864-3003<br>Phone: (850)-864-4004",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 4,
+    "zoom": 7,
     "html": "<b>Fort Walton Beach, FL</b></br>632B Lovejoy Rd NW #2, Fort Walton Beach, 32548</br>Fax: (850)-864-3003<br>Phone: (850)-864-4004"
   },
   {
@@ -56,7 +56,7 @@ var coastpumplocs = [
     "street": "4682 Dusk Ct, Jacksonville, FL 32207",
     "desc": "Fax: (904)-636-0735<br>Phone: (904)-636-0627",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 4,
+    "zoom": 7,
     "html": "<b>Jacksonville, FL</b></br>4682 Dusk Ct, Jacksonville, FL 32207</br>Fax: (904)-636-0735<br>Phone: (904)-636-0627"
   },
   {
@@ -66,7 +66,7 @@ var coastpumplocs = [
     "street": "3761 NW 16th St, Lauderhill, FL, 33311",
     "desc": "Phone: (954) 583-6202 <br>Fax: (954) 587-7096 <br>Mrg: Brian Thompson<br>Regional: Marlon Sirju",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 4,
+    "zoom": 7,
     "html": "<b>Lauderhill, FL</b></br>3761 NW 16th St, Lauderhill, FL, 33311</br>Phone: (954) 583-6202 <br>Fax: (954) 587-7096 <br>Mrg: Brian Thompson<br>Regional: Marlon Sirju"
   },
   {
@@ -76,7 +76,7 @@ var coastpumplocs = [
     "street": "10302 90th Trail, Live Oak, FL 32060",
     "desc": "Phone: (386) 208-0888<br>Fax: (386) 208-0684<br>Mgr: Brian Campbell <br>Regional Mgr: Brian Campbell",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 4,
+    "zoom": 7,
     "html": "<b>Live Oak, FL</b></br>10302 90th Trail, Live Oak, FL 32060</br>Phone: (386) 208-0888<br>Fax: (386) 208-0684<br>Mgr: Brian Campbell <br>Regional Mgr: Brian Campbell"
   },
   {
@@ -86,7 +86,7 @@ var coastpumplocs = [
     "street": "1652 Longwood Ave, Altamonte Springs, FL 32701",
     "desc": "Fax: (407)-331-0886<br>Phone: (407)-331-8989",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 4,
+    "zoom": 7,
     "html": "<b>Altamonte Springs, FL</b></br>1652 Longwood Ave, Altamonte Springs, FL 32701</br>Fax: (407)-331-0886<br>Phone: (407)-331-8989"
   },
   {
@@ -96,7 +96,7 @@ var coastpumplocs = [
     "street": "7835 NW 72nd Ave, Medley, (Miami) FL 33166-2215",
     "desc": "Phone: (305)883-0564 <br>Fax: (305)889-1475 <br>Mgr: Andy Alvarez <br>Regional Mgr: Marlon Sirju",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 4,
+    "zoom": 7,
     "html": "<b>Medley, (Miami) FL</b></br>7835 NW 72nd Ave, Medley, (Miami) FL 33166-2215</br>Phone: (305)883-0564 <br>Fax: (305)889-1475 <br>Mgr: Andy Alvarez <br>Regional Mgr: Marlon Sirju"
   },
   {
@@ -106,7 +106,7 @@ var coastpumplocs = [
     "street": "3576 Plover Ave, Naples FL 34117",
     "desc": "Phone: (239)-352-3520<br>Fax: (239)-348-3434<br>Mgr: Richard Sollmann<br>Regional Mgr: Ron Reagan",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 4,
+    "zoom": 7,
     "html": "<b>Naples, FL</b></br>3576 Plover Ave, Naples FL 34117</br>Phone: (239)-352-3520<br>Fax: (239)-348-3434<br>Mgr: Richard Sollmann<br>Regional Mgr: Ron Reagan"
   },
   {
@@ -116,7 +116,7 @@ var coastpumplocs = [
     "street": "2870 NE 24th St, Ocala, FL 34470",
     "desc": "Phone: (352)620-0616 <br>Fax: (352) 620-0982 <br>Mgr: Mike McGurk <br>Regional Mgr: Steve McCormack<br>Rick Davis Fax# (352) 629-5640",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 4,
+    "zoom": 7,
     "html": "<b>Ocala, FL</b></br>2870 NE 24th St, Ocala, FL 34470</br>Phone: (352)620-0616 <br>Fax: (352) 620-0982 <br>Mgr: Mike McGurk <br>Regional Mgr: Steve McCormack<br>Rick Davis Fax# (352) 629-5640"
   },
   {
@@ -126,7 +126,7 @@ var coastpumplocs = [
     "street": "140 Scarlet Blvd, Oldsmar, FL 34677",
     "desc": "Phone: (813)-855-2456<br>Fax: (813)-854-4834<br>Mgr: Jeff Reinig <br>Regional Mgr: John Salazar",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 4,
+    "zoom": 7,
     "html": "<b>Oldsmar, FL</b></br>140 Scarlet Blvd, Oldsmar, FL 34677</br>Phone: (813)-855-2456<br>Fax: (813)-854-4834<br>Mgr: Jeff Reinig <br>Regional Mgr: John Salazar"
   },
   {
@@ -136,7 +136,7 @@ var coastpumplocs = [
     "street": "1130 12th Ave E, Port Charlotte, FL 33980",
     "desc": "Phone: (941)-729-5618<br>Fax: (941)-772-0638<br>Mgr: Sal Rios<br>Regional Mgr: John Salazar",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 4,
+    "zoom": 7,
     "html": "<b>Port Charlotte, FL</b></br>1130 12th Ave E, Port Charlotte, FL 33980</br>Phone: (941)-729-5618<br>Fax: (941)-772-0638<br>Mgr: Sal Rios<br>Regional Mgr: John Salazar"
   },
   {
@@ -146,7 +146,7 @@ var coastpumplocs = [
     "street": "4241 James St, Port Charlotte, FL 33980",
     "desc": "Phone: (941)629-4234 <br>Fax: (941) 629-6714 <br>Mgr: Jacob Coleman <br>Regional Mgr: Ron Reagan",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 4,
+    "zoom": 7,
     "html": "<b>Port Charlotte, FL</b></br>4241 James St, Port Charlotte, FL 33980</br>Phone: (941)629-4234 <br>Fax: (941) 629-6714 <br>Mgr: Jacob Coleman <br>Regional Mgr: Ron Reagan"
   },
   {
@@ -156,7 +156,7 @@ var coastpumplocs = [
     "street": "555 NW Enterprise Dr, Port St. Lucie, FL 36986",
     "desc": "Phone: (772)340-3400 <br>Fax: (772) 340-3480 <br>Mgr: Eric White <br>Regional: Marlon Sirju",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 4,
+    "zoom": 7,
     "html": "<b>Port St. Lucie, FL</b></br>555 NW Enterprise Dr, Port St. Lucie, FL 36986</br>Phone: (772)340-3400 <br>Fax: (772) 340-3480 <br>Mgr: Eric White <br>Regional: Marlon Sirju"
   },
   {
@@ -166,7 +166,7 @@ var coastpumplocs = [
     "street": "2690 W Airport Blvd, Sanford, FL 32771",
     "desc": "Phone: (407) 328-0040 <br>Fax: (407) 323-8811 <br>Mgr: Craig Finnell <br>Regional Mgr: Phil Savage",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 4,
+    "zoom": 7,
     "html": "<b>Sanford, FL</b></br>2690 W Airport Blvd, Sanford, FL 32771</br>Phone: (407) 328-0040 <br>Fax: (407) 323-8811 <br>Mgr: Craig Finnell <br>Regional Mgr: Phil Savage"
   },
   {
@@ -176,7 +176,7 @@ var coastpumplocs = [
     "street": "1701 Cattlemen Rd, Sarasota, FL 34232",
     "desc": "Phone: (941)-378-9554<br>Fax: (941)-378-9470<br>Training Mgr: Nick Flickinger <br>Regional Mgr: John Salazar",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 4,
+    "zoom": 7,
     "html": "<b>Sarasota, FL</b></br>1701 Cattlemen Rd, Sarasota, FL 34232</br>Phone: (941)-378-9554<br>Fax: (941)-378-9470<br>Training Mgr: Nick Flickinger <br>Regional Mgr: John Salazar"
   },
   {
@@ -186,7 +186,7 @@ var coastpumplocs = [
     "street": "610 Groveland Ave, Venice, FL 34285",
     "desc": "Phone: (941)-484-3738<br>Fax: (941)-484-2631<br>Mgr: Josh Ferrill<br>Regional Mgr: Ron Reagan",
     "icon": "https://mt.googleapis.com/vt/icon/name=icons/onion/SHARED-mymaps-container-bg_4x.png,icons/onion/SHARED-mymaps-container_4x.png,icons/onion/1502-shape_star_4x.png&highlight=ff000000,FFD600",
-    "zoom": 4,
+    "zoom": 7,
     "html": "<b>Venice, FL</b></br>610 Groveland Ave, Venice, FL 34285</br>Phone: (941)-484-3738<br>Fax: (941)-484-2631<br>Mgr: Josh Ferrill<br>Regional Mgr: Ron Reagan"
   },
   {
@@ -196,7 +196,7 @@ var coastpumplocs = [
     "street": "1750 W 10th St, Riviera Beach, FL 33404",
     "desc": "Phone:(561)844-5252 <br>Fax: (561) 842-8842 <br>Mgr: Todd Willis <br>Regional: Marlon Sirju",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 4,
+    "zoom": 7,
     "html": "<b>Riviera Beach, FL</b></br>1750 W 10th St, Riviera Beach, FL 33404</br>Phone:(561)844-5252 <br>Fax: (561) 842-8842 <br>Mgr: Todd Willis <br>Regional: Marlon Sirju"
   },
   {
@@ -206,7 +206,7 @@ var coastpumplocs = [
     "street": "1652 S Ronald Reagan Blvd, Altamonte Springs, FL 32701",
     "desc": "Phone: (407) 331-8989 <br>Fax: (407) 331-0886 <br>Mgr: Ken Galgano <br>Regional Mgr: Phil Savage",
     "icon": "https://www.shareicon.net/download/32x32/2016/08/18/811941_miscellaneous_512x512.png",
-    "zoom": 4,
+    "zoom": 7,
     "html": "<b>Altamonte Springs, FL</b></br>1652 S Ronald Reagan Blvd, Altamonte Springs, FL 32701</br>Phone: (407) 331-8989 <br>Fax: (407) 331-0886 <br>Mgr: Ken Galgano <br>Regional Mgr: Phil Savage"
   }
 ]
